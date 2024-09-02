4c207333a19f08b74ad16fb101425803bb21298f
Shaun Inman <shaun@panic.com>
2024-09-02 14:08:16 -0400

    recompile based on existing dts not dtb files

--- dtb/recompile.sh
+++ dtb/recompile.sh
@@ -2,7 +2,7 @@
 set -x
 
 shopt -s nullglob  # Ensure that the loop doesn't run if no files match
-for FILE in *.dtb; do
+for FILE in *.dts; do
 	NAME="${FILE%.*}"
 	dtc -I dts -O dtb -o $NAME.dtb $NAME.dts 2> >(grep -v "Warning")
 done
